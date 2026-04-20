f3e7407a2d5a152fe00fc0a047defac6ebaca560
puneeth <gudipati.puneeth.35.3@mountblue.tech>
2026-04-20 20:42:49 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.set("trust proxy", true);
+
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
